a9b6282ee3eb8e1758aa9e556e4738a32f577703
akhilkmr716 <akhilkmr716@gmail.com>
2023-11-23 10:58:57 +0530

    QuerySelector && QuerySelectorALL

---
+++ Task4/dom.js
@@ -0,0 +1,19 @@
+// QUERYSELECTOR //
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.color = 'white';
+
+
+
+// QUERYSELECTORALL //
+var items = document.querySelectorAll('li');
+items[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'green' ;
+}
