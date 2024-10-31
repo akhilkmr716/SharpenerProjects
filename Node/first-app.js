ce2a98347039fa4b9a266f01fdd9c455d8a064c9
akhilkmr716 <akhilkmr716@gmail.com>
2024-10-31 20:34:32 +0530

    Basic Javascript concepts

--- Node/first-app.js
+++ Node/first-app.js
@@ -1,1 +1,11 @@
-console.log("Hello world!!");
+const mul = (a,b) => {return a*b};
+console.log(mul(5,6));
+const student = {
+    rollNo : 1,
+    name: "Akhil",
+    class: 12,
+    display(){
+        console.log(`The student's name is ${this.name} and his roll no is ${this.rollNo}, He studies in class ${this.class}`);
+    }
+}
+console.log(student.display());
