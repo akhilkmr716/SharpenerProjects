8e907edc82e4d71f56dd17ea46afde450be8b724
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-04 09:33:52 +0530

    Experimenting with Request/Response

--- Node/app.js
+++ Node/app.js
@@ -1,5 +1,26 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log("Akhil");
+    const url = req.url;
+    if(url === '/home'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('</html>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+    }
+    if(url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('</html>');
+        res.write('<body><h1>Welcome to About us Page!!!!</h1></body>');
+        res.write('</html>');
+    }
+    if(url === '/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('</html>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        
+    }
+   // process.exit();
+    
 });
-server.listen(4000);
+server.listen(3000);
