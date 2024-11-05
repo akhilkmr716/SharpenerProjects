ec81f1144c74d60432bc45968d2f97e7d82ccd6a
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-05 12:23:11 +0530

    Edited to read the file from data and render it on screen

--- Node/app.js
+++ Node/app.js
@@ -4,28 +4,34 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if(url === '/'){
+        const data = fs.readFileSync('./message.txt');
         res.setHeader('Content-Type', 'text/html');
         res.write('</html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
+        res.write(`<body><h1>${data}</h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>`);
         res.write('</html>');
         return res.end();
     }
     if(url === '/message' && method === 'POST'){
         const body = [];
         req.on('data',(chunk) => {
-            console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('./message.txt',message);
-            console.log(parsedBody);
+            fs.writeFile('./message.txt',message,() => {
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
         })
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
     }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(3000);        
