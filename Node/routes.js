58ac3bbecb2a0e496b3fb54b54b7b90909ac3c36
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-07 12:26:10 +0530

    Getting Started with Express

--- Node/routes.js
+++ Node/routes.js
@@ -20,7 +20,7 @@ const requestHandler = (req, res) => {
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
+            const message = parsedBody.split('=')[1];
             fs.writeFile('./message.txt',message,() => {
                 res.statusCode = 302;
                 res.setHeader('Location','/');
