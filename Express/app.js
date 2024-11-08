bc0fe1ecc32be3f6082348792a033a6343a0637e
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-08 19:11:47 +0530

    Routes and Filter

--- Express/app.js
+++ Express/app.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-//app.use('/favicon.ico', (req, res) => res.status(204).end());
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="product" method="POST"><input type="text" name="title"><input type="number" name="productsize"><button type="submit">Add Product</button></form>');
-});
+app.use("/admin", adminRoutes);
+app.use("/shop", shopRoutes);
 
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    res.send("<h1>Hello from Express!!!!</h1>");
-});
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+}); //By default the path is set to "/" if not explicitly mentioned
 
 app.listen(3000);
