4599ba8ba564e2e7615e8cd15198a8b97157d413
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-10 21:07:53 +0530

    Serving HTML, CSS and JS(Views)

--- Express/routes/shop.js
+++ Express/routes/shop.js
@@ -1,9 +1,23 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require("../util/path");
+
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.send("<h1>Hello from Express!!!!</h1>");
+    console.log(__dirname);
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+router.get('/contactus',(req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contactus.html'));
+});
+
+router.post('/success',(req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'form-submit.html'));
 });
 
 module.exports = router;
