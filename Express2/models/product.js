034d58a7796ca66447a5f31e27215ce6dbae5931
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-12 16:42:05 +0530

    My version: Storing data into file implementation of model

---
+++ Express2/models/product.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require("../util/path");
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        //products.push(this);
+        //console.log(JSON.stringify(this));
+        fs.appendFile(path.join(rootDir, "data", "product.txt"), JSON.stringify(this) + "\n", (err) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            console.log('Data appended to File!!!');
+        });
+    }
+
+    static fetchAll() {
+        try {
+            const data = fs.readFileSync(path.join(rootDir, "data", "product.txt"));
+            const products = data.toString().trim().split("\n").map((e) => JSON.parse(e));
+            return products;
+        } catch(err) {
+            console.error(err);
+        }
+    }
+}
