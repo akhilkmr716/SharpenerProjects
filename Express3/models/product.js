68db1abf4efd314fb129bbc2e2f734c2f1aeabbc
akhilkmr716 <akhilkmr716@gmail.com>
2024-11-15 14:23:31 +0530

    Dynamic Routes part 3( Edit and Delete functionality)

--- Express3/models/product.js
+++ Express3/models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,31 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
+      if(this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+        });
+      }
+    });
+  }
+
+  static delete(id) {
+    getProductsFromFile(products => {
+      const existingProductIndex = products.findIndex(prod => prod.id === id);
+      products.splice(existingProductIndex, 1);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
-      });
+        });
     });
   }
 
