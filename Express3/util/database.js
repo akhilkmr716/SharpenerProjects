c97ca49720affe575c8412faac415f78dae17c22
akhilkmr716 <akhilkmr716@gmail.com>
2024-12-06 18:49:43 +0530

    Retrieving data from databases

--- Express3/util/database.js
+++ Express3/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'ChowChow1#'
 });
 
 module.exports = pool.promise();
